74f71a781eaff623a53da8e907c373df15338978
Maycon Rogerio Tenorio de Albuquerque <CIANDT\mayconrt@lnb022018cps.ciandt.global>
2021-08-30 10:28:53 -0300

    feat: add endpoint health check

--- server.js
+++ server.js
@@ -2,10 +2,18 @@ import express from 'express';
 import { getIp } from './src/infra/os.js';
 const app = express();
 
+app.use(express.urlencoded({extended:true}));
+app.use(express.json());
+
+
 const ip = await getIp();
 
+app.use('/health', (req, res) => {
+    res.status(200).json({message: 'server is health'})
+})
+
 app.use('/', (req, res) => {
-    res.send('Hellow word! My Ip address is ' + ip);
+    res.status(200).json({message: 'Hellow word! My Ip address is ' + ip})
 })
 
 app.listen(3000, () => {
